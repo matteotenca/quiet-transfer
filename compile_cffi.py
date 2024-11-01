ea4d8555bb305d6a00e6af7f55c9c078926d34b6
Matteo Tenca <93343338+matteotenca@users.noreply.github.com>
2024-11-01 13:06:46 +0100

    remove portaudio dependency, add new Windows precompiled libs
    add new compile sequence for cibuildwheel on linux and macOS x86

--- compile_cffi.py
+++ compile_cffi.py
@@ -21,14 +21,12 @@ from pathlib import Path
 
 ffibuilder = FFI()
 if sys.platform.find("win32") >= 0:
-    ffi_include = Path().absolute().joinpath("include_win32").joinpath("quiet_portaudio_all_cleaned_cffi.h").as_posix()
+    ffi_include = Path().absolute().joinpath("include_win32").joinpath("quiet_cffi.h").as_posix()
     with open(ffi_include) as h_file:
         ffibuilder.cdef(h_file.read())
         ffibuilder.set_source("quiettransfer._quiettransferwin32",  # name of the output C extension
                               """
-                                    #include "portaudio.h"
-                                    #include "quiet_portaudio_all_cleaned.h"
-                                    
+                                    #include "quiet.h"
                                     """,
                               libraries=["quiet", "jansson", "fec", "liquid"],
                               include_dirs=[Path().absolute().joinpath("include_win32").as_posix()],
